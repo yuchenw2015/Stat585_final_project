eea938f6e3af191bc633a239107bbb41447047ae
yuchenw2015 <37629284+yuchenw2015@users.noreply.github.com>
2019-04-09 12:42:11 -0500

    optimize plot function

--- Function.R
+++ Function.R
@@ -3,12 +3,13 @@ library(ggplot2)
 library(ggmap)
 library(maps)
 library(mapdata)
-#这里目前一共有7个function，其中有2个是读数据，另外5个是画图。
-#还可以创造出更多的画图function
-#可以run到258行（run所有到function定义），
-#然后运行几个例子（从260行起）
+#这里目前一共有3个function，其中有2个是读数据，另外1个是画图。
+
+
 
 #Function1:read the data of one location
+#id 是机场代码，type是天气要素
+#此函数可以读取一个机场的若干天气要素
 current_weather <- function(id, type){
   assertthat::assert_that(is.character(id))
   assertthat::assert_that(stringr::str_length(id) == 4)
@@ -32,8 +33,6 @@ current_weather <- function(id, type){
         longitude  <- as.numeric(text[i])
       else if(name[i] == "observation_time")
         observation_time <- text[i]
-      else if(name[i] == "weather")
-        weather <- text[i]
       else if(name[i] == type){
         data <- text[i]
         mark <- 1
@@ -41,8 +40,8 @@ current_weather <- function(id, type){
         
     }
     obs <- data.frame(location, station_id, latitude, longitude, observation_time,
-                      weather, data)
-    colnames(obs)[7] <- type 
+                      data)
+    colnames(obs)[6] <- type 
   }
   if (m > 1){
     for (i in 1:n){
@@ -56,8 +55,6 @@ current_weather <- function(id, type){
         longitude  <- as.numeric(text[i])
       else if(name[i] == "observation_time")
         observation_time <- text[i]
-      else if(name[i] == "weather")
-        weather <- text[i]
       else {
         for (k in 1:m){
           if(name[i] == type[k]){
@@ -67,11 +64,10 @@ current_weather <- function(id, type){
         }
       }
     }
-    obs <- data.frame(location, station_id, latitude, longitude, observation_time,
-                      weather)  
+    obs <- data.frame(location, station_id, latitude, longitude, observation_time)  
     for (i in 1:m)
       obs <- cbind(obs, data[i])
-    names(obs)[7:(6+m)] <- type
+    names(obs)[6:(6+m-1)] <- type
   }
   if(sum(mark) < m)
     print("Some of your types are not found! They are shown as NA!")
@@ -80,6 +76,7 @@ current_weather <- function(id, type){
 }
 
 #Function2:read the data of several different locations
+#读取多个机场的多个天气要素
 current_weather_more <- function(id_vector, type){
   n <- length(id_vector)
   obs <- NULL
@@ -90,90 +87,28 @@ current_weather_more <- function(id_vector, type){
   obs
 }
 
-
-#Function 3: plot temp_f on a US map
-plot_temp_f <- function(id_vector, label = T){
-  data <- current_weather_more(id_vector, "temp_f")
-  data$temp_f <- as.numeric(as.character(data$temp_f))
-  states <- map_data("state")
-  if (label == T){
-    ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group),
-                   color = "grey", alpha = 0.6) + 
-      geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = temp_f)) +
-      geom_text(data = data, 
-                aes(x = longitude, y = latitude, label = station_id),
-                size = 2)+
-      theme_bw() +
-      labs(title = "Temperature in F", 
-           subtitle = as.character(data$observation_time[1]))+
-      coord_fixed(1.3) +
-      guides(fill = FALSE)
-  }else{
-    ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group),
-                   color = "grey", alpha = 0.6) + 
-      geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = temp_f)) +
-      theme_bw() +
-      labs(title = "Temperature in F", 
-           subtitle = as.character(data$observation_time[1]))+
-      coord_fixed(1.3) +
-      guides(fill = FALSE)
+#Function3: plot an element
+#label 是否标出机场code
+#number 此天气要素是否是numeric
+#此函数可以画出多个机场的同一个天气要素
+plot_weather <- function(id_vector, type, label = T, number = F){
+  data <- current_weather_more(id_vector, type)
+  colnames(data)[6]<-"element"
+  if (number == T){
+    data$element <- as.numeric(as.character(data$element))
   }
-}
-
-#Function 4: plot temp_c on a US map
-plot_temp_c <- function(id_vector, label = T){
-  data <- current_weather_more(id_vector, "temp_c")
-  data$temp_c <- as.numeric(as.character(data$temp_c))
-  states <- map_data("state")
-  if (label == T){
-    ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group),
-                   color = "grey", alpha = 0.6) + 
-      geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = temp_c)) +
-      geom_text(data = data, 
-                aes(x = longitude, y = latitude, label = station_id),
-                size = 2)+
-      theme_bw() +
-      labs(title = "Temperature in C", 
-           subtitle = as.character(data$observation_time[1]))+
-      coord_fixed(1.3) +
-      guides(fill = FALSE)
-  }else{
-    ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group),
-                   color = "grey", alpha = 0.6) + 
-      geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = temp_c)) +
-      theme_bw() +
-      labs(title = "Temperature in C", 
-           subtitle = as.character(data$observation_time[1]))+
-      coord_fixed(1.3) +
-      guides(fill = FALSE)
-  }
-}
-
-
-#Function 5: plot wind_mph
-plot_wind_mph <- function(id_vector, label = T){
-  data <- current_weather_more(id_vector, "wind_mph")
-  data$wind_mph <- as.numeric(as.character(data$wind_mph))
   states <- map_data("state")
   if(label == T){
     ggplot(data = states) + 
       geom_polygon(aes(x = long, y = lat, group = group),
                    color = "grey", alpha = 0.6) + 
       geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = wind_mph)) +
+                                 color = element)) +
       geom_text(data = data, 
                 aes(x = longitude, y = latitude, label = station_id),
-                size = 2)+
+                size = 1.5)+
       theme_bw() +
-      labs(title = "Wind Speed in mph", 
+      labs(title = type, 
            subtitle = as.character(data$observation_time[1]))+
       coord_fixed(1.3) +
       guides(fill = FALSE)
@@ -182,81 +117,15 @@ plot_wind_mph <- function(id_vector, label = T){
       geom_polygon(aes(x = long, y = lat, group = group),
                    color = "grey", alpha = 0.6) + 
       geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = wind_mph)) +
+                                 color = element)) +
       theme_bw() +
-      labs(title = "Wind Speed in mph", 
-           subtitle = as.character(data$observation_time[1]))+
-      coord_fixed(1.3) +
-      guides(fill = FALSE)
-  }
-  
-}
-
-
-#Function 6:plot wind_kt
-plot_wind_kt <- function(id_vector, label = T){
-  data <- current_weather_more(id_vector, "wind_kt")
-  data$wind_kt <- as.numeric(as.character(data$wind_kt))
-  states <- map_data("state")
-  if(label == T){
-    ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group),
-                   color = "grey", alpha = 0.6) + 
-      geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = wind_kt)) +
-      geom_text(data = data, 
-                aes(x = longitude, y = latitude, label = station_id),
-                size = 2)+
-      theme_bw() +
-      labs(title = "Wind Speed in kt", 
-           subtitle = as.character(data$observation_time[1]))+
-      coord_fixed(1.3) +
-      guides(fill = FALSE)
-  }else{
-    ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group),
-                   color = "grey", alpha = 0.6) + 
-      geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = wind_kt)) +
-      theme_bw() +
-      labs(title = "Wind Speed in kt", 
+      labs(title = type, 
            subtitle = as.character(data$observation_time[1]))+
       coord_fixed(1.3) +
       guides(fill = FALSE)
   }
 }
 
-#Function 7:plot wind_kt
-plot_wind_dir <- function(id_vector, label = T){
-  data <- current_weather_more(id_vector, "wind_dir")
-  states <- map_data("state")
-  if(label == T){
-    ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group),
-                   color = "grey", alpha = 0.6) + 
-      geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = wind_dir)) +
-      geom_text(data = data, 
-                aes(x = longitude, y = latitude, label = station_id),
-                size = 2)+
-      theme_bw() +
-      labs(title = "Wind direction", 
-           subtitle = as.character(data$observation_time[1]))+
-      coord_fixed(1.3) +
-      guides(fill = FALSE)
-  }else{
-    ggplot(data = states) + 
-      geom_polygon(aes(x = long, y = lat, group = group),
-                   color = "grey", alpha = 0.6) + 
-      geom_point(data = data,aes(x = longitude, y = latitude,
-                                 color = wind_dir)) +
-      theme_bw() +
-      labs(title = "Wind direction", 
-           subtitle = as.character(data$observation_time[1]))+
-      coord_fixed(1.3) +
-      guides(fill = FALSE)
-  }
-}
 
 #example for reading data
 current_weather("KAMW", c("wind_mph", "temp_f", "haha"))
@@ -264,8 +133,6 @@ current_weather("KAMW", "temp_c")
 current_weather_more(c("KAMW", "KAIO", "KCID", "KCNC"), c("temp_f"))
 
 #example for plots
-plot_temp_f(c("KAMW", "KAIO", "KCID", "KCNC"))
-plot_temp_c(c("KAMW", "KAIO", "KCID", "KCNC"), label = F)
-plot_wind_mph(c("KAMW", "KAIO", "KCID", "KCNC"))
-plot_wind_kt(c("KAMW", "KAIO", "KCID", "KCNC"), label = F)
-plot_wind_dir(c("KAMW", "KAIO", "KCID", "KCNC"), label = F)
+plot_weather(c("KAMW", "KAIO", "KCID", "KCNC"), 
+                  type = "weather", 
+                  label = T, number = F)
